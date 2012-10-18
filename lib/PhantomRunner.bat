02dc0b754de87553ddcf02643f8024246cb1ff5e
Austin Rasmussen <ar@austinrasmussen.com>
2012-10-17 20:09:41 -0500

    Updated Exit Code

--- lib/PhantomRunner.bat
+++ lib/PhantomRunner.bat
@@ -1,8 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
+set errlvl=0
 for /r %%f in (%1\*) do (
  set filename=%%~nf
  if NOT "!filename:~0,1!" == "_" (
- PhantomJs\phantomjs.exe %1\_CasperRunner.js CasperJs\ %%f
+  PhantomJs\phantomjs.exe "%1\_CasperRunner.js" CasperJs\ "%%f"
+  set /a errlvl=!errlvl! + !errorlevel!
  )
 )
+exit /b !errlvl!
